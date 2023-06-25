ae987248a3ccb35a5c037f72d6c4575a24b7f707
Anastasia Shcheblykina <anastasia23121993@gmail.com>
2023-06-25 21:22:33 +0300

    changed test

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,6 +2,8 @@ module.exports = {
     launch: {
         headless: false,
         defaultViewport: null,
+        slowmo: 300,
+        devtools: true,
         args: ['--start-maximized'] //— используем максимальный размер окна браузера
       },
     
